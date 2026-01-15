dc7fc4f3e5115616da8c34dc7aec7f90650c4cd5
Niewei <neewii.yang@outlook.com>
2026-01-15 15:00:33 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -179,7 +179,7 @@ const dark_vanilla = 'rgb(228,212,220)';
 // If your map has an offset please change this line
 // issues #92 and #198
 export const NEED_FIX_MAP = false;
-export const MAIN_COLOR = nike;
+export const MAIN_COLOR = '#f97316';
 export const PROVINCE_FILL_COLOR = '#47b8e0';
 export const COUNTRY_FILL_COLOR = dark_vanilla;
 
