a3cee03602a1adcb801fa7173d9d5416d5ac19c3
cheirehang <123562171+cherie-hang@users.noreply.github.com>
2026-01-16 21:28:50 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,8 +15,8 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Neewii的Sports Map',
-  siteUrl: 'https://neewii-running-page.vercel.app/',
+  siteTitle: 'Weihang的Sports Map',
+  siteUrl: 'https://weihang-running-page.vercel.app/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
